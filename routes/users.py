b2a50d6187241ca716000f3e766366a6108df0fa
DeSiReLeSsOn <theproblemdi@gmail.com>
2024-08-19 03:46:54 +0300

    add user.email unique validator

--- routes/users.py
+++ routes/users.py
@@ -1,2 +1,22 @@
 from fastapi import APIRouter, HTTPException, status
-from models.user import User, UserSignIn
+from models.users import User, UserSignIn
+
+
+user_router = APIRouter(
+    tags=["User"]
+)
+
+
+users = {}
+
+@user_router.post("/singup")
+async def sign_new_user(data: NewUser) ->dict:
+    if data.email in users:
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT, 
+            details="User with supplid username exists"
+        )
+    users[data.email] = data 
+    return {
+        "message": "user successfully registered!"
+    }
