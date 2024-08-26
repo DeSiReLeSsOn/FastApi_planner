816d0174e867c44710998b20890653cf1215bb88
DeSiReLeSsOn <theproblemdi@gmail.com>
2024-08-26 23:24:50 +0300

    change database settings

--- models/users.py
+++ models/users.py
@@ -1,15 +1,25 @@
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel, EmailStr 
+from typing import Optional, List
 
+from beanie import Document, Link
+
+from models.events import Event
 
 class User(BaseModel):
     email: EmailStr
     password: str 
+    events: Optional[List[Link[Event]]]
+
+
+    class Settings:
+        name = "users"
 
     class Config:
         json_schema_extra = {
             "example": {
                 "email": "fastapi@packt.com",
                 "password": "strong!!!",
+                "events": [],
             }
         }
 
@@ -19,10 +29,10 @@ class UserSignIn(BaseModel):
     password: str 
 
 
-    class Config:
-        json_schema_extra = {
-            "example": {
-                "email": "fastapi@packt.com",
-                "password": "strong!!!"
-            }
-        }
+    # class Config:
+    #     json_schema_extra = {
+    #         "example": {
+    #             "email": "fastapi@packt.com",
+    #             "password": "strong!!!"
+    #         }
+    #     }
