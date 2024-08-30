968e8ae9b5c3c31d414553f89dd34b8a7dd36ff7
DeSiReLeSsOn <theproblemdi@gmail.com>
2024-08-30 15:09:38 +0300

    testing..

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI 
 from fastapi.responses import RedirectResponse
+from fastapi.middleware.cors import CORSMiddleware
 
 
 from routes.users import user_router 
@@ -14,6 +15,18 @@ app = FastAPI()
 
 
 settings = Settings()
+
+
+# regsiter origins 
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 #Regsiter routes 
