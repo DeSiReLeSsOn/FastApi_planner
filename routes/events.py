e70dbe7b77cee27f8d7d9974f28d29136c6390d4
DeSiReLeSsOn <theproblemdi@gmail.com>
2024-08-30 16:18:53 +0300

    testing routes

--- routes/events.py
+++ routes/events.py
@@ -49,7 +49,7 @@ async def create_event(body: Event, user: str = Depends(authenticate)) -> dict:
 
 
 
-@event_router.put("/edit/{id}", response_model=Event)
+@event_router.put("/{id}", response_model=Event)
 async def update_event(id: PydanticObjectId, 
                        body: EventUpdate, 
                        user: str = Depends(authenticate)) -> Event:
